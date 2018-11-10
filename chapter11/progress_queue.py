c33ec57f79c752ab3265b5af773a4893e1e42e52
alvin <alvinyeats@gmail.com>
2018-11-10 11:14:23 +0800

    init

---
+++ chapter11/progress_queue.py
@@ -0,0 +1,26 @@
+# _*_ coding: utf-8 _*_
+
+import time
+from multiprocessing import Process, Queue
+
+
+def producer(queue):
+    queue.put("a")
+    time.sleep(2)
+
+
+def consumer(queue):
+    time.sleep(2)
+    data = queue.get()
+    print(data)
+
+
+if __name__ == "__main__":
+    queue = Queue(10)
+    my_producer = Process(target=producer, args=(queue,))
+    my_consumer = Process(target=consumer, args=(queue,))
+    my_producer.start()
+    my_consumer.start()
+    my_producer.join()
+    my_consumer.join()
+
